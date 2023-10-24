18b00b369229bef37262550567e535ef13d86acc
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-10-24 11:48:21 +0200

    primer comit despues del fallo

--- NumeroCuadrado/controladores/Program.cs
+++ NumeroCuadrado/controladores/Program.cs
@@ -7,15 +7,19 @@ namespace NumeroCuadrado
         public static void Main(string[] args)
         {
             MenuYNumeroInterfaz myn = new MenuYNumeroImplementacion();
-            int numeroint = myn.MenuYNum();
+            int numeroInt = myn.MenuYNum();
 
-            if ((Math.Sqrt(numeroint) % 2)== 0 )
+            int numeroCuadrado = Convert.ToInt32(Math.Sqrt(numeroInt));
+            int operacion = numeroCuadrado * numeroCuadrado;
+           
+
+            if (operacion==numeroInt)
             {
-                Console.WriteLine(Math.Sqrt( numeroint ));
+                Console.WriteLine("El numero que elevado al cuadrado da " + numeroInt + " es: " + numeroCuadrado);
             }
             else
             {
-                Console.WriteLine("No tiene raiz entera");
+                Console.WriteLine("No tiene numero que elevado al cuadrado de: " + numeroInt);
             }
         }
     }
